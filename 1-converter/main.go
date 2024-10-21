5efdbee9076387384d6b5f234c0dd99ed19022ba
MikVG <miggl@mail.ru>
2024-10-21 14:42:34 +0300

    fix const

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const usdToEur = 0.92
 	const usdToRub = 97.0
-	eurToRub := usdToEur * usdToRub
+	const eurToRub = usdToEur * usdToRub
 
 	fmt.Println("EUR в RUB:", eurToRub)
 
